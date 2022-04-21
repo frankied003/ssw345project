3d8db6a8b74632f1c1c2493eab74629561834a01
Frankie DiGiacomo <frankied003@hotmail.com>
2022-04-21 16:40:56 -0400

    Set up basic discord bot

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+require('dotenv').config();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+})
+
+client.login(process.env.TOKEN)
